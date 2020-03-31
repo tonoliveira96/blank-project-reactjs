6d97187f21525d53848588969e3d2eae63b61ddc
tonoliveira96 <ton.oliveira@sempre.facig.edu.br>
2020-03-31 08:47:10 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="App" >
+      <h1 >
+      <p>
+          Hello!! Eu sou um prjeto reactJS em branco.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Fique a vontade para me editar
+        </p>
+      </h1>
     </div>
   );
 }
